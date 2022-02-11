482e3ce81dc75f235197402933d35e3ccc397d1c
maltaweel <m.altaweel@ucl.ac.uk>
2022-02-11 16:54:53 +0000

    commit

--- src/scrape_BM.py
+++ src/scrape_BM.py
@@ -13,6 +13,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import certifi
 import ssl
+import scrape_coins.Scrape as Scrape
 
 context = ssl._create_unverified_context()
 
@@ -23,69 +24,113 @@ pn=os.path.abspath(__file__)
 pn=pn.split("src")[0]  
 directory=os.path.join(pn,'data')
 
+#output path for csv
 filename=os.path.join(pn,'output','output_bm.csv')
+
+#fieldnames to print out in the csv output
 fieldnames=['id','description','denomination','image']
 
-def loadData():
-    #open individual files
-    for f in listdir(directory):
-                
-            #should only be .csv files
-        if 'collections' not in f:
-            continue
+class BM_Scrape(Scrape):
     
-        with open(os.path.join(directory,f),'r') as csvfile:
-            reader = csv.DictReader(csvfile)
+    '''
+    Method to load data from /data/ folder
+    '''
+    def loadData(self):
         
-        
+        #open individual files
+        for f in listdir(directory):
+                
+            #should only be .csv files and have collections in the title
+            if 'collections' not in f:
+                continue
+            
+            #open the output file and write it
             with open(filename, 'w') as csvf:
                 writer = csv.DictWriter(csvf, fieldnames=fieldnames,delimiter=',', quotechar='"')
-                writer.writeheader()   
+                writer.writeheader()  
                 
+                #read the file 
+                with open(os.path.join(directory,f),'r') as csvfile:
+                    reader = csv.DictReader(csvfile) 
+                        #read the rows of data
+                    for row in reader:
+                        try:
+                            
+                            #get the image link
+                            image=row['Image']
+                            #objType=row['Object type']
+                            
+                            #museum number
+                            mNumber=row['Museum number']
+                            
+                            #description
+                            description=row['Description']
+                            
+                            #coin denomination
+                            denomination=row['Denomination']
                 
-                #read the rows of data
-                for row in reader:
-                    try:
-                        image=row['Image']
-                        #objType=row['Object type']
-                        mNumber=row['Museum number']
-                        description=row['Description']
-                        denomination=row['Denomination']
-                
-                        img_folder=os.path.join(pn,'images_bm')
-                        downloadImage(image,'',img_folder)
+                            #create the output folder to put it in
+                            img_folder=os.path.join(pn,'images_bm')
+                            
+                            #download the image
+                            self.downloadImage(image,'',img_folder)
                     
-                        writeOutput(writer,description,denomination,mNumber,image.split('/'))
-                        csvf.flush()
+                            #write out the descriptive data
+                            self.writeOutput(writer,description,denomination,mNumber,image.split('/'))
+                            
+                            #flush the output to the file
+                            csvf.flush()
                     
-                    except Exception as e:
-                        print(e)
-                        continue
-                        
-                
-
-def downloadImage(img,name2,folder):
+                        except Exception as e:
+                            print(e)
+                            continue
     
-    download_img = urlopen(img,context=context)
-    name=img.split('/')
-    name=name[len(name)-1].replace('preview_','')
-    path_to_data=os.path.join(folder,name2+name)
+    '''
+    Method for downloading images from a given link.
+    @param img- image data
+    @param name2- second name to add to the image name
+    @param folder- the folder to put the image in
+    '''
+    def downloadImage(self,img,name2,folder):
     
-    txt = open(path_to_data, "wb")
-    
-    #write the binary data
-    txt.write(download_img.read())
-   
-    
-def writeOutput(writer,content,denomination,idd, l2):
-    image_name=l2[len(l2)-1]
-    image_name=image_name.replace('preview_','')
-    writer.writerow({'id': idd,'description':content,'denomination':denomination,'image':image_name})
-    
+        #the image to download is downloaded here
+        download_img = urlopen(img,context=context)
         
+        #split the name; create the name for the file
+        name=img.split('/')
+        name=name[len(name)-1].replace('preview_','')
+        path_to_data=os.path.join(folder,name2+name)
+    
+        #open the image data
+        txt = open(path_to_data, "wb")
+    
+        #write the binary data
+        txt.write(download_img.read())
+    
+    '''
+    Method to scrape information from links within a site (uri).
+    @param uri- the uri link to get data from
+    @param folder- the folder to put data in
+    @param denomination- the denomination of the coin type
+    @param writer- the writer to write results
+    @param csvf- the csv file to write the outputs
+    '''
+    def writeOutput(self, writer,content,denomination,idd, l2):
+        #get the name by removing the web address and keeping the last part
+        image_name=l2[len(l2)-1]
+        
+        #replace the preview_ part
+        image_name=image_name.replace('preview_','')
+        
+        #write the data
+        writer.writerow({'id': idd,'description':content,'denomination':denomination,'image':image_name})
+    
+'''
+The main to run the module.
+'''        
 def main():
-   
-    loadData()
+    scrap_bm=BM_Scrape()
+    scrap_bm.loadData()
     print('run')
 
 
